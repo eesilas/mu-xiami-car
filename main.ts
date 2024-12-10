df39c5b15b637474160babc376317bf2bb6ee29d
Learn STEM by Doing <32452061+eesilas@users.noreply.github.com>
2024-12-10 16:12:55 +0800

    Traffic card recognition.

--- main.ts
+++ main.ts
@@ -1,3 +1,18 @@
+muvisionAT.wifiSet("EVI-LINKSYS", "88888888", MuAtMode.ModeAP)
+muvision.begin(MuId.Mu00, MuVsMode.kI2CMode)
+muvision.visionBegin(MuId.Mu00, MuStatus.Enable, MuVsMessageVisionType.kVisionTrafficCard)
 basic.forever(function () {
-	
+    if (muvision.trafficCardType(MuId.Mu00, muvision.TrafficCardType.TrafficCardForward)) {
+    	
+    } else if (muvision.trafficCardType(MuId.Mu00, muvision.TrafficCardType.TrafficCardLeft)) {
+    	
+    } else if (muvision.trafficCardType(MuId.Mu00, muvision.TrafficCardType.TrafficCardRight)) {
+    	
+    } else if (muvision.trafficCardType(MuId.Mu00, muvision.TrafficCardType.TrafficCardPark)) {
+    	
+    } else if (muvision.trafficCardType(MuId.Mu00, muvision.TrafficCardType.TrafficCardTURN_AROUND)) {
+    	
+    } else {
+    	
+    }
 })
