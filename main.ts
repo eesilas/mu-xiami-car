a77722ef2ae2f95a76f28cb4b78490343c9fe23c
Learn STEM by Doing <32452061+eesilas@users.noreply.github.com>
2024-12-10 16:26:40 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,10 @@
-muvisionAT.wifiSet("EVI-LINKSYS", "88888888", MuAtMode.ModeAP)
 muvision.begin(MuId.Mu00, MuVsMode.kI2CMode)
 muvision.visionBegin(MuId.Mu00, MuStatus.Enable, MuVsMessageVisionType.kVisionTrafficCard)
+muvisionAT.wifiSet("EVI-LINKSYS", "88888888", MuAtMode.ModeAP)
 basic.forever(function () {
     if (muvision.trafficCardType(MuId.Mu00, muvision.TrafficCardType.TrafficCardForward)) {
-    	
+        xiamiBoard.motorRun(MOTOR.M1, DIRECTION.CW, 0)
+        xiamiBoard.motorRun(MOTOR.M2, DIRECTION.CW, 0)
     } else if (muvision.trafficCardType(MuId.Mu00, muvision.TrafficCardType.TrafficCardLeft)) {
     	
     } else if (muvision.trafficCardType(MuId.Mu00, muvision.TrafficCardType.TrafficCardRight)) {
