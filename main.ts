b8d608f16e15200500554d12807530ddf47ba846
Learn STEM by Doing <32452061+eesilas@users.noreply.github.com>
2024-12-10 16:28:29 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
 muvision.begin(MuId.Mu00, MuVsMode.kI2CMode)
 muvision.visionBegin(MuId.Mu00, MuStatus.Enable, MuVsMessageVisionType.kVisionTrafficCard)
+muvision.cameraSetAwb(MuId.Mu00, MuVsCameraWhiteBalance.kAutoWhiteBalance)
+muvision.cameraSetZoom(MuId.Mu00, MuVsCameraZoom.kZoomDefault)
 muvisionAT.wifiSet("EVI-LINKSYS", "88888888", MuAtMode.ModeAP)
 basic.forever(function () {
     if (muvision.trafficCardType(MuId.Mu00, muvision.TrafficCardType.TrafficCardForward)) {
