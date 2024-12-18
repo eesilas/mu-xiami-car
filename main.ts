fa65f7711a6472eaac0a32cfc155930ad9617b3c
Learn STEM by Doing <32452061+eesilas@users.noreply.github.com>
2024-12-18 16:23:50 +0800

    Control by number card.

--- main.ts
+++ main.ts
@@ -1,23 +1,23 @@
 let var_res = 0
 muvision.begin(MuId.Mu00, MuVsMode.kI2CMode)
-muvision.visionBegin(MuId.Mu00, MuStatus.Enable, MuVsMessageVisionType.kVisionTrafficCard)
+muvision.visionBegin(MuId.Mu00, MuStatus.Enable, MuVsMessageVisionType.kVisionNumberCard)
 muvision.cameraSetAwb(MuId.Mu00, MuVsCameraWhiteBalance.kAutoWhiteBalance)
 muvision.cameraSetZoom(MuId.Mu00, MuVsCameraZoom.kZoomDefault)
 muvisionAT.wifiSet("EVI-LINKSYS", "88888888", MuAtMode.ModeAP)
 basic.forever(function () {
-    if (muvision.trafficCardType(MuId.Mu00, muvision.TrafficCardType.TrafficCardForward)) {
+    if (muvision.numberCardType(MuId.Mu00, muvision.NumCardType.NumCard1)) {
         xiamiBoard.motorRun(MOTOR.M1, DIRECTION.CW, 0)
         xiamiBoard.motorRun(MOTOR.M2, DIRECTION.CW, 0)
-    } else if (muvision.trafficCardType(MuId.Mu00, muvision.TrafficCardType.TrafficCardLeft)) {
+    } else if (muvision.numberCardType(MuId.Mu00, muvision.NumCardType.NumCard2)) {
         xiamiBoard.motorRun(MOTOR.M1, DIRECTION.CW, 65)
         xiamiBoard.motorRun(MOTOR.M2, DIRECTION.CW, 45)
-    } else if (muvision.trafficCardType(MuId.Mu00, muvision.TrafficCardType.TrafficCardRight)) {
+    } else if (muvision.numberCardType(MuId.Mu00, muvision.NumCardType.NumCard3)) {
         xiamiBoard.motorRun(MOTOR.M1, DIRECTION.CW, 45)
         xiamiBoard.motorRun(MOTOR.M2, DIRECTION.CW, 65)
-    } else if (muvision.trafficCardType(MuId.Mu00, muvision.TrafficCardType.TrafficCardPark)) {
+    } else if (muvision.numberCardType(MuId.Mu00, muvision.NumCardType.NumCard4)) {
         xiamiBoard.motorRun(MOTOR.M1, DIRECTION.CW, 0)
         xiamiBoard.motorRun(MOTOR.M2, DIRECTION.CW, 0)
-    } else if (muvision.trafficCardType(MuId.Mu00, muvision.TrafficCardType.TrafficCardTURN_AROUND)) {
+    } else if (muvision.numberCardType(MuId.Mu00, muvision.NumCardType.NumCard5)) {
         xiamiBoard.motorRun(MOTOR.M1, DIRECTION.CCW, 65)
         xiamiBoard.motorRun(MOTOR.M2, DIRECTION.CCW, 65)
         basic.pause(500)
